ad854b2ec2b076af3fd7ff447b20bc3fd34cc0e5
temrecelik <temrecelik21@gmail.com>
2024-12-27 01:13:08 +0300

    IUnitOFWork created.

---
+++ AuthServer.CoreLayer/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.CoreLayer.UnitOfWork
+{
+   public interface IUnitOfWork
+    {
+        /*
+         *CommitAsync metodu ile veri tabanına asenkron olarak kayıt işlemi yapılır.Savechange concrete classında savechange metodu
+         *kullanılacağı için burada isim karışıklığı olmaması için CommitAsync olarak isimlendirildi.
+         */
+        Task CommitAsync();
+        void Commit();
+    }
+}
