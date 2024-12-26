57b83e60a9a39b6b75c7fe2ac580b249552e2fc4
temrecelik <temrecelik21@gmail.com>
2024-12-26 15:07:29 +0300

    ErrorDto and Response Dto created for using all APIs

---
+++ AuthServer.Shared/Dtos/ErrorDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Shared.Dtos
+{
+    /*
+     *ErrorDto ile API'lerde oluşan hatalar için dönecek bir hatalar dto tipinde döndürülürse kullanıcıya daha ölçeklendirilebilinir bir 
+      deneyim sunulur.Bu hatalar validation hataları, token süresi hataları gibi hatalar olabilir. Hatalar tüm API'lerde ortak bir yapı 
+      olduğu için shared  klasöründe tutmak mantıklıdır.
+
+     *Property'lerin set kısmını private'a çektiğimizde kullanıcı direkt olarak bir dto'yu oluşturamaz oluşturması için constructor methodları
+      çağırması gereklidir.
+    */
+    public class ErrorDto
+    {
+        public List<string> Errors { get; private set; }
+
+        /*
+        Validation hataları kullanıcya gösterilirken bazı hatalar sadece geliştiricinin görmesi gereken hatalardır. Bu nedenle
+        Ishow property'si hatanın kullanıcıya gösterilip gösterilmemesini tutar.
+        */
+        public bool IsShow {  get; private set; }
+
+      
+        //bu constuctor ile bir adet ErrorDto nesnesi oluşturup property'lerini bu nesne üzerinden verebiliriz.
+        public ErrorDto() { 
+            Errors = new List<string>();
+        }
+
+        //hatalar bir list değilde tek bir hata olarak eklenirse bu hata için oluşacak Dto'nun constructor methodu
+        public ErrorDto(string error,bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<string> error, bool isShow)
+        {
+            Errors = error;
+            IsShow =isShow;
+        }
+    }
+}
---
+++ AuthServer.Shared/Dtos/Response.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Shared.Dtos
+{
+    /*
+     *Response Dto'su API'lerde yazılan end-point'lerin dönüş veri tipi olarak kullanılır.Normalde bir end-point işlemini gerçekleştirdikten
+     sonra herhangi bir veri tipi dönebilir ve ya hiçbir şey dönmeyebilir. Ancak client tarafını kodlayan bir yazılımcı bu end-point'i 
+     kullanacağı için ilgili end-point'in parametreleri ve döneceği veriler anlaşılır olmalıdır. Bu nedenle tüm end-point'ler aynı tip  
+     dto'yu dönerse bu dto'nun içine ilgili end-point için varsa hata ve hatanın kodu varsa ilgili Entity eklenerek direkt bu dto'yu dönmek 
+     client tarafı için oldukça yararladır. Bu dto'yu projedeki tüm API'ler kullanacağı için shared projesinde oluşturulması mantıklıdır.
+
+    *Generic olmasının nedeni her end-point için kullanabilir olmasıdır. Diyelimki data olarak product entity'si dönülecek o zaman data'nın 
+     Product olması gereklidir.
+
+     */
+    public class Response<T> where T : class
+    {
+        public T? Data { get; private set; }
+
+        /*
+        status code client'ın anlayacağı kodlardan oluşur. End-point'e koduna göre status kod başarılı ya da başarısız şekilde girilerek 
+        Response dönülür.
+        */
+        public int StatusCode { get; private set; } 
+
+
+        /*
+         Response olarak eğer bir hata dönülmek istenirse oluşturduğumuz ErroDto'yu da response'a ekleyerek hatada dönebiliriz. Bu alan 
+        nullable'dır yani her zaman bir end-point hata dönmez ancak hata döndüğü yerlerde vardır.
+         */
+        public ErrorDto? Error { get; private set; }
+
+        /*
+        Örneğin ProductController'da bulunun bir end-point için başarılı bir response dönmek istersek
+        return  Response<Product>.Success(Product,2001) şeklinde bir dönüş yapabiliriz. Success methodu direkt olarak static olduğu için
+        Response classı üzerinden nesne üretmeden çağırabiliriz. Aynı methodu birden fazla yazıp parametrelerin sayısını farklı vermek 
+        o methodu overload etmek demektir. Örnek verecek olursak bir end-point succeed olarak bir response döner ancak data dönmek istemeyebilir
+        o zaman success method overload ederek iki veya daha fazla oluşturulabilir.
+        */
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode };
+        }
+
+        public static Response<T> Success(int statusCode) { 
+            return new Response<T> {Data= default, StatusCode = statusCode };
+        }
+
+        //Response olarak birden fazla hata mesajı dönülecekse ErrorDto olarak dönülebilir.
+        public static Response<T> Fail(int statusCode, ErrorDto error) { 
+            return new Response<T> { StatusCode = statusCode, Error = error,  };
+        }
+
+        /*
+         Fail methodunu overload ettik eğer bir tane hata mesajı var ise bunu string olarak olarak ErrorDto'ya çevirerek Fail response
+        static methodunu oluşturduk
+        */
+        public static Response<T> Fail(int statusCode,  string ErrorMessage ,bool ErrorIsShow)
+        {
+
+            return new Response<T> { StatusCode = statusCode, Error = new ErrorDto(ErrorMessage,ErrorIsShow) };
+        }
+
+    }
+}
